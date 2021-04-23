497e6b5827aedf9206f609dc84b94e2a05ef841d
Juan Lagos <jlagosobando@gmail.com>
2021-04-22 22:12:54 -0400

    Update 0414_PFEx.java

--- 06 PA/0414_PFEx.java
+++ 06 PA/0414_PFEx.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.mycompany.icc7002;
 
 import java.util.ArrayList;
 import java.util.Arrays;
