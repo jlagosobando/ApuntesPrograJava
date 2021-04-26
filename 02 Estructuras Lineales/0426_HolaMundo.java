2f8ff5ae6ee99222a838a42a9d03532e9efe8414
Juan Lagos <jlagosobando@gmail.com>
2021-04-26 13:22:26 -0400

    Update and rename HolaMundo.java to 0426_HolaMundo.java

---
+++ 02 Estructuras Lineales/0426_HolaMundo.java
@@ -0,0 +1,21 @@
+/**
+ *
+ * @author Juan
+ */
+public class HolaMundo {
+    
+    public static void main(String[] args){
+    
+    //escribir "Hola Mundo"
+    System.out.println("Hola Mundo");
+    System.out.println("Hola Mundo");
+    System.out.print("Hola Mundo");
+    System.out.print("Hola Mundo");
+    System.out.println("Hola Mundo");
+    System.out.println("Hola Mundo");
+    
+        
+    
+    }
+    
+}
