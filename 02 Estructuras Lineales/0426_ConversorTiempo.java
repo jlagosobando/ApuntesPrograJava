e5862457eb9cddcf2971ccdbfe065be16624866c
Juan Lagos <jlagosobando@gmail.com>
2021-04-26 13:38:14 -0400

    Update 0426_ConversorTiempo.java

--- 02 Estructuras Lineales/0426_ConversorTiempo.java
+++ 02 Estructuras Lineales/0426_ConversorTiempo.java
@@ -24,22 +24,7 @@ public class ConversorTiempo {
         //salida
         System.out.println("Los " + segundos + " segundos, equivalen a " + min + " minutos y " + seg + " segundos");
 
-        //min y seg >10 escribir min,":",seg
-        //min<10 y seg>10 escribir "0",min,":",seg
-        //min>10 y seg<10 escribir min,":0",seg   
-        //min y seg <10 escribir "0",min,":0",seg
-        // if(condicion){acciones}
-        // else {acciones} 
-        
-        if (min < 10 && seg < 10) {
-            System.out.println("0" + min + ":0" + seg);
-        } else if (min < 10) {
-            System.out.println("0" + min + ":" + seg);
-        } else if (seg < 10) {
-            System.out.println(min + ":0" + seg);
-        } else if (min > 10 && seg > 10) {
-            System.out.println(min + ":" + seg);
-        }
+  
         
       
 
