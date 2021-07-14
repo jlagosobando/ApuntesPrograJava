8d71fd2bab060be1bd0956149f9fd494ed78ee72
Juan Lagos <jlagosobando@gmail.com>
2021-07-14 13:22:43 -0400

    icc700-07-14

---
+++ 05 POO/POO0712/src/main/java/com/mycompany/poo211/Administrador.java
@@ -0,0 +1,54 @@
+package com.mycompany.poo211;
+
+
+public class Administrador extends Persona{
+    //atributos
+    private int nTrabajadores;
+    // plantear la existencia de una clase trabajador 
+    // trabajador es hija de persona
+    // trabajador es padre de admnistrador, vendedor y cajero
+    // la clave es : trabajador tiene algo que lo distingue de persona
+    // el administrador tendrá un arreglo de trabajadores, que son los que están a su cargo
+    
+    
+    
+    //constructores
+    public Administrador(String nombre, String gen, byte edad, String rut, int nTrabajadores) {
+        super(nombre, gen, edad, rut);
+        this.nTrabajadores=nTrabajadores;
+    }
+    
+    // crear constructor de acuerdo al nuevo escenario con la clase trabajador
+    
+    //set-get
+    public int getNTrabajadores(){
+        return nTrabajadores;
+    }
+    public void setNTrabajadores(int trabajadores){
+        this.nTrabajadores = trabajadores;
+    }
+    
+    
+    // AQUÍ HAY ENVIDIA DE CLASES
+    // ESTOS METODOS DEBIESEN SER DE STOCK
+    
+    // estamos seguros que stock es int ?
+    // el stock está siempre actualizado, el supuesto Stock[][] será modificado
+    
+    public void comprobarStock(int stockTotal, int stockVendido){
+        int stockSobrante = stockTotal-stockVendido;
+        System.out.println("el stock restante es "+stockSobrante);
+    }
+     
+   // revisarStock() > mostrar el stock actual
+   // modificarStock(admin, parametros) > resta/suma cant. de producto determinado  
+   // parametros podrían ser un número(codigo del producto), el producto, una cantidad de producto  
+    
+   // entonces que hace el administrador?
+    
+    @Override
+      public void saludar() {
+        System.out.println("Hola! Buen día, soy el administrador");
+    }
+     
+}
---
+++ 05 POO/POO0714/src/main/java/com/mycompany/poo211/Cajero.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.poo211;
+
+/**
+ *
+ * @author Juan
+ */
+public class Cajero extends Vendedor {
+
+    //atributos
+    private int nCaja;
+
+    //const
+    public Cajero() {
+        //super("negro");
+        super();
+        this.setUniforme("negro");
+    }
+    
+    
+    public Cajero(String nombre, String gen, byte edad, String rut, int hrsSemanales, int nCaja) {
+        super(nombre, gen, edad, rut,"negro",hrsSemanales);
+        this.nCaja=nCaja;
+        
+    }
+
+    //set-get
+    public int getNCaja() {
+        return nCaja;
+    }
+    
+    public void setNCaja(int nvaCaja){
+    this.nCaja=nvaCaja;
+    }
+    
+    // es parte de yuna transaccion, o
+    // cobrar requiere una transaccion como parametro
+    public void cobrar(int total, int pago){
+    int vuelto=pago-total;
+    System.out.println("Tome, su vuelto es de: "+vuelto);
+    }
+    
+    
+    
+
+    
+}
