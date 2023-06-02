c95f1b0b033a11f18688a94e785cc7be35c0d0b6
Juan Lagos <jlagosobando@gmail.com>
2023-06-02 15:29:11 -0400

    Update 0505_Fibo_Test.java

--- 06 PA/0505_Fibo_Test.java
+++ 06 PA/0505_Fibo_Test.java
@@ -1,66 +1,79 @@
-package com.example.testeo;
 //cuidado con el copy paste para cambiar el nombre del package
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import static com.example.dev.Main_Fibo.*;
 
-public class Fibo_Test {
+import static org.example.Main_fibo.*;
 
-//se corrigió en codigo detalle de escritura en nombre de variable
+
+
+
+public class Main_test {
+
     @Test
-    public void Test_Fibo1() {
-        int num_control, num_obtenido;
-        num_control = 0;
-        num_obtenido = Gen_Fibo(1);
+    public void test1(){
+    int num_control, num_obtenido;
+    num_control=0;
+    num_obtenido=Gen_Fibo(1);
+    Assertions.assertEquals(num_control, num_obtenido);
+}
 
+    @Test
+    public void test2(){
+        int num_control, num_obtenido;
+        num_control=1;
+        num_obtenido=Gen_Fibo(2);
         Assertions.assertEquals(num_control, num_obtenido);
     }
 
     @Test
-    public void Test_Fibo2() {
-        int num_control, num_obtenido;
-        num_control = 1;
-        num_obtenido = Gen_Fibo(2);
-
-        Assertions.assertEquals(num_control, num_obtenido);
-    }
-
-    @Test
-    public void Test_Fibo3(){
+    public void test3(){
         int num_control, num_obtenido;
         num_control=1;
         num_obtenido=Gen_Fibo(3);
-
         Assertions.assertEquals(num_control, num_obtenido);
-
     }
 
     @Test
-    public void Test_Fibo4(){
+    public void test4(){
         int num_control, num_obtenido;
         num_control=2;
         num_obtenido=Gen_Fibo(4);
-
-        Assertions.assertEquals(num_control,num_obtenido);
+        Assertions.assertEquals(num_control, num_obtenido);
     }
 
     @Test
-    public void Test_Fibo5(){
+    public void test5(){
         int num_control, num_obtenido;
         num_control=3;
         num_obtenido=Gen_Fibo(5);
+        Assertions.assertEquals(num_control, num_obtenido);
+    }
 
-        Assertions.assertEquals(num_control,num_obtenido);
+    @Test
+    public void test6(){
+        int num_control, num_obtenido;
+        num_control=5;
+        num_obtenido=Gen_Fibo(6);
+        Assertions.assertEquals(num_control, num_obtenido);
+    }
 
+    @Test
+    public void testX(){
+        int num_control, num_obtenido;
+        num_control=21;
+        num_obtenido=Gen_Fibo(9);
+        Assertions.assertEquals(num_control, num_obtenido);
     }
 
     @Test
     public void Test_Serie(){
-        int[] serie_control = {0,1,1,2,3,5,8};
-        int[] serie_obtenida = Serie_Fibo(serie_control.length);
+       int[] serie_control= {0,1,1,2,3,5,8};
+       int[] serie_obtenida = Serie_Fibo(serie_control.length);
 
-        Assertions.assertArrayEquals(serie_control,serie_obtenida);
+       Assertions.assertArrayEquals(serie_control,serie_obtenida);
+
     }
 
 
 }
+
