3bd611d0b78f20eee8749a5a5cbf7a7d22c59363
Juan Lagos <jlagosobando@gmail.com>
2023-06-02 15:27:33 -0400

    Update 0505_Main_Fibo.java

--- 06 PA/0505_Main_Fibo.java
+++ 06 PA/0505_Main_Fibo.java
@@ -1,31 +1,31 @@
-package com.example.dev;
+package org.example;
 
-import java.util.ArrayList;
-
-// 1,2     3,4,5,6,7
-// 0,1 ... 1,2,3,5,8..
-public class Main_Fibo {
+public class Main_fibo {
+    public static void main(String[] args) {
 
 
-
-//generador sg posicion
-public static int Gen_Fibo(int posicion){
-if(posicion==1) return 0;
-if(posicion==2) return 1;
-else
-    return Gen_Fibo(posicion-2)+Gen_Fibo(posicion-1);
-}
-
-
-
-public static int[] Serie_Fibo(int cantidad){
-    int[] serie_prod = new int[cantidad];
-    int i;
-
-    for(i=0;i<cantidad;i++){
-        serie_prod[i]=Gen_Fibo(i+1);
     }
-return serie_prod;
-}
 
-}
+    public static int Gen_Fibo(int posicion) {
+        if(posicion==1) return 0;
+        if(posicion==2) return 1;
+        else
+            return Gen_Fibo(posicion-2)+Gen_Fibo(posicion-1);
+    }
+
+
+
+
+    public static int[] Serie_Fibo(int cantidad){
+        int[] serie_prod=new int[cantidad];
+
+        for(int i=1;i<=cantidad;i++){
+            serie_prod[i-1]=Gen_Fibo(i);
+        }
+        return serie_prod;
+
+    }
+
+
+
+    }
