7d32901bda216169e359c40ccafc1831d5307250
Juan Lagos <jlagosobando@gmail.com>
2023-06-02 17:40:09 -0400

    Update 0930_Main_Pass.java

--- 06 PA/0930_Main_Pass.java
+++ 06 PA/0930_Main_Pass.java
@@ -1,26 +1,26 @@
-package com.example.dev;
+
+ /**
+ * com.example.dev Paquete de desarrollo de la solución
+ */
+// -encoding UTF-8 -charset UTF-8 -docencoding UTF-8
 
 public class Main_Pass {
-    public static String Gen_Pass() {
-
+    public static String Gen_Pass(){
         String may = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String min = may.toLowerCase();
         String nums = "0123456789";
         String alphanums = may + min + nums;
-        int i;
 
         int randomLength = 6 + (int) (Math.random() * 4);
         StringBuilder pass = new StringBuilder(randomLength);
-
         pass.append(may.charAt((int)(Math.random() * may.length())));
         pass.append(nums.charAt((int)(Math.random() * nums.length())));
 
-        for (i = 2; i < randomLength; i++) {
-            pass.append(alphanums.charAt((int)(Math.random() * alphanums.length())));
-        }
 
-        System.out.println(">>>" + pass.toString());
+        for(int i=2;i<randomLength;i++)
+        {
+            pass.append(alphanums.charAt((int) (Math.random() * alphanums.length())));
+        }
         return pass.toString();
     }
 }
-
